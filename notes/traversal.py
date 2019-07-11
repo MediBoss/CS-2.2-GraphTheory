8993806dcc4413922d7d4cd5608ec92334cf3860
Medi Assumani <mediassumani49@gmail.com>
2019-07-10 18:09:36 -0700

    tree traversal 1 : DFS

---
+++ notes/traversal.py
@@ -0,0 +1,31 @@
+
+def dfs_recursive(graph, start, visited=None):
+    
+    if not visited:
+        visited = set()
+
+    visited.add(start)
+
+    for neighbor in graph[start]:
+
+        if neighbor not in visited:
+            dfs_recursive(graph, neighbor, visited)
+
+        continue
+    
+    return visited
+
+def main():
+    
+    graph = {}
+    graph["A"] = ["E", "B"]
+    graph["E"] = ["A", "C"]
+    graph["C"] = ["E", "F"]
+    graph["F"] = ["C", "D"]
+    graph["D"] = ["F", "B"]
+    graph["B"] = ["D", "A"]
+
+    print(dfs_recursive(graph, "A"))
+
+if __name__ == "__main__":
+    main()
