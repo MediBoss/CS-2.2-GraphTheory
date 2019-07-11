034f2a97f46f3e9a8b7abdde2f13bf7f69bb1430
Medi Assumani <mediassumani49@gmail.com>
2019-07-11 14:40:17 -0700

    traversal

--- notes/traversal.py
+++ notes/traversal.py
@@ -1,19 +1,39 @@
 
-def dfs_recursive(graph, start, visited=None):
+def dfs(graph, start, visited=None):
     
     if not visited:
         visited = set()
-
     visited.add(start)
 
     for neighbor in graph[start]:
-
         if neighbor not in visited:
-            dfs_recursive(graph, neighbor, visited)
-
+            dfs(graph, neighbor, visited)
         continue
-    
+
     return visited
+
+def find_path(graph, origin, destination, visited=None):
+    
+    if not visited:
+        visited = set()
+    
+    visited.add(origin)
+
+    # Base case
+    if origin == destination:
+        return True
+
+    for neighbor in graph[origin]:
+        if neighbor not in visited:
+            # Recursive case
+            print(neighbor)
+            find_path(graph, neighbor, destination, visited)
+
+
+    return False
+
+def bfs(graph):
+    pass
 
 def main():
     
@@ -25,7 +45,10 @@ def main():
     graph["D"] = ["F", "B"]
     graph["B"] = ["D", "A"]
 
-    print(dfs_recursive(graph, "E"))
-
+    #dfs(graph, "A")
+    origin = "A"
+    destination = "F"
+    #print("Is there path from {} to {} : ".format(origin, destination), find_path(graph, origin, destination))
+    find_path(graph, origin, destination)
 if __name__ == "__main__":
     main()
