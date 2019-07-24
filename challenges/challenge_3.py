bf7b38f241d5d88df065cc7d9e17fcef5e952691
Medi Assumani <mediassumani49@gmail.com>
2019-07-24 15:50:59 -0700

    [Update] Challenge 3, Dijstra

--- challenges/challenge_3.py
+++ challenges/challenge_3.py
@@ -22,40 +22,55 @@ def depth_first_search(graph, origin, destination, visited=None):
                 
     return False
 
-def find_shortest_path(graph, source):
+def find_shortest_path(graph, source): # Code extracted from Interview Cacke
     
     # Set the initial distance to the source to 0 and all to infinity
     weight_to_get_to = { node : float('inf') for node in graph }
     weight_to_get_to[source] = 0
 
     # Keep track of nodes that's already dequeued - which means we've already found the shortest path to them
-    dequeued_nodes = set()
+    visited_nodes = set()
 
-    # Priority queue ordering nodes by the weight to get to them
+    # Initializing & Creating the priority Queue
     priority_queue = []
     for node in graph:    
         heapq.heappush(priority_queue, (weight_to_get_to[node], node)) # heapq builds an array of tuples with a node and its weight
-    
+
     while len(priority_queue) > 0:
         # deque the next node from the priority queue
-        cheapest_cost, cheapest_node = heapq.heappop(priority_queue)
+        _, cheapest_node = heapq.heappop(priority_queue)
         # Add the dequeued node in the set of visited nodes
-        dequeued_nodes.add(cheapest_node)
+        visited_nodes.add(cheapest_node)
 
         # Visit the neighbors of the dequeued nodes
-        for neighbor, weight in graph[cheapest_cost]:
+        for neighbor, weight in graph[cheapest_node]:
 
-            # Skip the neighbor that's seen already
-            if neighbor in dequeued_nodes:
+            # Skip the neighbor that's visited already
+            if neighbor in visited_nodes:
                 continue
             
             # Can we get there cheapely via the neighbor
             if weight_to_get_to[cheapest_node] + weight < weight_to_get_to[neighbor]:
-
                 # Update the cost the cost to reach the node
                 weight_to_get_to[neighbor] = weight_to_get_to[cheapest_node] + weight
 
                 # upadte the cost to reach this node in the priority queue
-                heapq.heapreplace()
+                heapq.heapreplace(priority_queue, (weight_to_get_to[neighbor], neighbor))
 
     return weight_to_get_to
+
+
+
+def main():
+    
+    graph = {}
+
+    graph["A"] = [("B", 10), ("D", 11), ("C", 2)]
+    graph["B"] = [("A", 10), ("C", 7)]
+    graph["C"] = [("B", 7), ("D", 6), ("A", 2)]
+    graph["D"] = [("A", 11), ("C", 6)]
+
+    print(find_shortest_path(graph, "A"))
+
+if __name__ == "__main__":
+    main()
